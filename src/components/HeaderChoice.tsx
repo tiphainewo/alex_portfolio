0432eebfa53cd0934fb4b10e5b92c5ffd78c625e
tiphainewo <tiphainewo@gmail.com>
2025-02-18 15:36:44 +0100

    added animation plugin (edited some aspects manually, might need to revert to older version if not fixed)

--- src/components/HeaderChoice.tsx
+++ src/components/HeaderChoice.tsx
@@ -8,11 +8,11 @@ function HeaderChoice({ title, link }: { title: string; link: string }) {
   }, [location]);
 
   return (
-    <div className="flex items-center flex-col">
+    <div className="flex items-center flex-col group">
       <NavLink to={link} className={location.pathname === link && "font-bold-shadow"}>
         {title}
       </NavLink>
-      <div className={`transition-all bg-purple rounded-full h-1 ${location.pathname === link ? "w-6 " : "w-0"}`}></div>
+      <div className={` bg-purple rounded-full h-1 ${location.pathname === link ? "w-6 motion-preset-pop" : "w-0 group-hover:w-2"}`}></div>
     </div>
   );
 }
